89bee253249e5bce41afa07b6868e18d383a4575
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-01-27 13:11:53 +0100

    Namespace detector trait name changed in Laravel 5.4

--- src/Prettus/Repository/Generators/NamespaceDetectorTrait.php
+++ src/Prettus/Repository/Generators/NamespaceDetectorTrait.php
@@ -2,7 +2,7 @@
 
 namespace Prettus\Repository\Generators;
 
-use Illuminate\Console\AppNamespaceDetectorTrait;
+use Illuminate\Console\DetectsApplicationNamespace;
 
 /**
  * Class NamespaceDetectorTrait.
@@ -11,7 +11,7 @@ use Illuminate\Console\AppNamespaceDetectorTrait;
  */
 trait NamespaceDetectorTrait
 {
-    use AppNamespaceDetectorTrait;
+    use DetectsApplicationNamespace;
 
     /**
      * Get root namespace.
