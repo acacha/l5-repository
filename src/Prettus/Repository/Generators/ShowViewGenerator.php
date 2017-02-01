bd5b83789130f7d4011baa1574f0a4b2587ef7f0
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-02-01 19:03:52 +0100

    Added all views

---
+++ src/Prettus/Repository/Generators/ShowViewGenerator.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Prettus\Repository\Generators;
+
+/**
+ * Class IndexViewGenerator.
+ *
+ * @package Prettus\Repository\Generators
+ */
+class IndexViewGenerator extends Generator
+{
+
+    /**
+     * Get stub name.
+     *
+     * @var string
+     */
+    protected $stub = 'views/index';
+
+    /**
+     * Get generator path config node.
+     *
+     * @return string
+     */
+    public function getPathConfigNode()
+    {
+        return 'views';
+    }
+
+    /**
+     * Get destination path for generated file.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->getBasePath() . '/' . parent::getConfigGeneratorClassPath($this->getPathConfigNode(), true)
+                . '/' . $this->getPluralName() . '/index.blade.php';
+    }
+
+    /**
+     * Get base path of destination file.
+     *
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return config('repository.generator.basePath', app_path());
+    }
+
+    /**
+     * Get array replacements.
+     *
+     * @return array
+     */
+    public function getReplacements()
+    {
+        return [
+            'plural'            => $this->getPluralName(),
+            'singular'          => $this->getSingularName(),
+        ];
+    }
+
+    /**
+     * Gets plural name based on model
+     *
+     * @return string
+     */
+    public function getPluralName()
+    {
+        return str_plural(lcfirst(ucwords($this->getClass())));
+    }
+
+    /**
+     * Gets singular name based on model
+     *
+     * @return string
+     */
+    public function getSingularName()
+    {
+        return str_singular(lcfirst(ucwords($this->getClass())));
+    }
+}
+
---
+++ src/Prettus/Repository/Generators/ViewGenerator.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Prettus\Repository\Generators;
+
+/**
+ * Class EditViewGenerator.
+ *
+ * @package Prettus\Repository\Generators
+ */
+class EditViewGenerator extends Generator
+{
+
+    /**
+     * Get stub name.
+     *
+     * @var string
+     */
+    protected $stub = 'views/view';
+
+    /**
+     * Get generator path config node.
+     *
+     * @return string
+     */
+    public function getPathConfigNode()
+    {
+        return 'views';
+    }
+
+    /**
+     * Get destination path for generated file.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->getBasePath() . '/' . parent::getConfigGeneratorClassPath($this->getPathConfigNode(), true)
+                . '/' . $this->getPluralName() . '/view.blade.php';
+    }
+
+    /**
+     * Get base path of destination file.
+     *
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return config('repository.generator.basePath', app_path());
+    }
+
+    /**
+     * Get array replacements.
+     *
+     * @return array
+     */
+    public function getReplacements()
+    {
+        return [
+            'plural'            => $this->getPluralName(),
+            'singular'          => $this->getSingularName(),
+        ];
+    }
+
+    /**
+     * Gets plural name based on model
+     *
+     * @return string
+     */
+    public function getPluralName()
+    {
+        return str_plural(lcfirst(ucwords($this->getClass())));
+    }
+
+    /**
+     * Gets singular name based on model
+     *
+     * @return string
+     */
+    public function getSingularName()
+    {
+        return str_singular(lcfirst(ucwords($this->getClass())));
+    }
+}
+
